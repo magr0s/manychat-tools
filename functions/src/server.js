7845594bd370eeb7630830aea1f110580aca84f1
Oleksandr Krokhmalnyi <magr0s.ak@gmail.com>
2020-03-02 15:48:15 +0200

    build(app): Transition to firebase

---
+++ functions/src/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const api = require('./api/routes')
+
+const app = express()
+
+app.use(bodyParser.json())
+  .use(cors({ origin: true }))
+  .use('/', api)
+  .get('*', (_, res) => (
+    res.status(404)
+      .json({
+        success: false,
+        data: 'Error: Endpoint not found.'
+      })
+  ))
+
+module.exports = app
--- src/index.js
+++
@@ -1,5 +0,0 @@
-const api = require('./api/routes')
-
-module.exports = {
-  api
-}
